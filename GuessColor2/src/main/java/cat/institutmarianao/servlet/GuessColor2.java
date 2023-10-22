35a16ff8f0eb63e07fd748e989800f1d48c25700
“andres02” <rebe5723@gmail.com>
2023-10-22 16:09:17 +0200

    blud

--- GuessColor2/src/main/java/cat/institutmarianao/servlet/GuessColor2.java
+++ GuessColor2/src/main/java/cat/institutmarianao/servlet/GuessColor2.java
@@ -1,57 +1,50 @@
-
 package cat.institutmarianao.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class GuessColor2 extends HttpServlet {
-        private static final long serialVersionUID = 1L;
-        
-        private String initColor;
-        
-        @Override
-        public void init() throws ServletException {
-        	// S'ha configurat un paràmetre que conté el color a endevinar:
-            initColor = getServletConfig().getInitParameter("color");
-        }
+	private static final long serialVersionUID = 1L;
 
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response)
-                        throws ServletException, IOException {
-                processRequest(request, response);
-        }
+	private String initColor;
 
-        protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-                        throws ServletException, IOException {
-                response.setContentType("text/html;charset=UTF-8");
+	@Override
+	public void init() throws ServletException {
+		// S'ha configurat un paràmetre que conté el color a endevinar:
+		initColor = getServletConfig().getInitParameter("color");
+	}
 
-                try (PrintWriter out = response.getWriter()) {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
 
-                        out.println("<!DOCTYPE html>");
-                        out.println("<html>");
-                        out.println("<head>");
-                        out.println("<title>Guess the color</title>");
-                        out.println("</head>");
-                        out.println("<body>");
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
 
-                        String result = "Sorry, you lost!";
+		try (PrintWriter out = response.getWriter()) {
 
-                        // L'usuari ha seleccionat un color i ho ha enviat.
-                        String paramColor = request.getParameter("color");
+			// L'usuari ha seleccionat un color i ho ha enviat.
+			String paramColor = request.getParameter("color");
 
-                        if (initColor.toLowerCase().equals(paramColor.toLowerCase())) {
-                                result = "Congratulations! You guess the color";
-                        }
-
-                        out.println("<h1>" + result + "</h1>");
-                        out.println("<a href='GuessColor.html'>Try again<a/>");
-                        out.println("</body>");
-                        out.println("</html>");
-                }
-        }
+			if (paramColor == null) {
+				RequestDispatcher rs = request.getRequestDispatcher("GuessColor2.html");
+				rs.forward(request, response);
+			} else if (initColor.equalsIgnoreCase(paramColor.toLowerCase())) {
+				RequestDispatcher rs = request.getRequestDispatcher("guanyar.html");
+				rs.forward(request, response);
+			}else {
+				RequestDispatcher rs = request.getRequestDispatcher("perder.html");
+				rs.forward(request, response);
+			}
+		}
+	}
 }
