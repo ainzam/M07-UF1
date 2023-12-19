355566647c1958f89acee26f9b71eb0d78623945
cfgs <cfgs@pc-info-host>
2023-12-19 17:11:44 +0100

    blud

--- UF2/M07UF2-Pt-STUDENTS-master/orders/src/main/java/cat/institutmarianao/controller/LoginController.java
+++ UF2/M07UF2-Pt-STUDENTS-master/orders/src/main/java/cat/institutmarianao/controller/LoginController.java
@@ -2,24 +2,30 @@ package cat.institutmarianao.controller;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 
 // TODO - Configure Spring element and add mappings
+@Controller
 public class LoginController {
+	
+	@GetMapping(value = "/check")
 	public String check(HttpServletRequest request) throws ServletException, IOException {
 		if (request.isUserInRole("ROLE_ADMIN")) {
 			return "redirect:/admin/orders";
 		}
 		return "redirect:/users/orders";
 	}
-
+	@GetMapping(value = "/login")
 	public String login() {
 		return "login";
 	}
-
+	
+	@GetMapping(value = "/loginfailed")
 	public String loginFailed(Model model) {
 		model.addAttribute("error", true);
 		return "login";
