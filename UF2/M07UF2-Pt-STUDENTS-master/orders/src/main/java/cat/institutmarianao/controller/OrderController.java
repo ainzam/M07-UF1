6033d733477efc1f07702ee9b9cd27be9374e39d
cfgs <cfgs@pc-info-host>
2024-01-16 17:05:16 +0100

    blud

--- UF2/M07UF2-Pt-STUDENTS-master/orders/src/main/java/cat/institutmarianao/controller/OrderController.java
+++ UF2/M07UF2-Pt-STUDENTS-master/orders/src/main/java/cat/institutmarianao/controller/OrderController.java
@@ -7,20 +7,32 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import cat.institutmarianao.domain.Order;
 import cat.institutmarianao.domain.User;
+import cat.institutmarianao.repository.ItemRepository;
+import cat.institutmarianao.repository.OrderRepository;
 
 //TODO - Configure Spring element and add mappings
 @Controller
+@RequestMapping("/users/orders")
 public class OrderController {
+	
 	@Autowired
-	private UserDetailsService userDetailsService;
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
 	@ModelAttribute("order")
 	public Order setupOrder() {
@@ -32,25 +44,31 @@ public class OrderController {
 		order.setClient(client);
 		return order;
 	}
-	
+	@GetMapping
 	public ModelAndView orders() {
 		// TODO - get authenticated user here
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		String username = authentication.getName();
-		User client = (User) userDetailsService.loadUserByUsername(username);
-		
+        String username = authentication.getName();
+        User client = (User) userDetailsService.loadUserByUsername(username);
+
 		// TODO - get user orders
-		
-		
+        Iterable<Order> orders = orderRepository.findByUser(client);
+		 
 		// TODO - Prepare the orders.jsp view and send user orders and Order.STATES as
 		// parameter
+        ModelAndView modelAndView = new ModelAndView("orders");
+        modelAndView.addObject("orders", orders);
+        modelAndView.addObject("states", Order.STATES);
 		return null;
 	}
 
+	@GetMapping("/newOrder")
 	public ModelAndView newOrder() {
 		// TODO - Prepare the newOrder.jsp view and send all the available items
 		// TODO - The new user order is in session
-		return null;
+		ModelAndView modelAndView = new ModelAndView("newOrder");
+        modelAndView.addObject("items", itemRepository.getAll());
+        return modelAndView;
 	}
 
 	public String newOrderClearItems(@SessionAttribute("order") Order order) {
